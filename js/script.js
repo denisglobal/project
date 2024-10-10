5cbd97b8ae7a09ce4ddcf373cfeac2b15250a6af
denis <example@gmail.com>
2024-10-11 01:51:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const sheesh = confirm("Ti pidor?");
+// console.log(sheesh);
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+// console.log(answers);
+let incr = 10;
+decr = 10;
+
+incr++;
+decr--;
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = true,
+  isClosed = false;
+
+console.log(isChecked && !isClosed);
